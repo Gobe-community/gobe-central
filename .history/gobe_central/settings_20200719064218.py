6aa51e93e90010c6f888b931d919fd2b0379f45e
everybees <jibola.bak@gmail.com>
2020-07-19 10:44:36 +0100

    added swagger documentation

--- .history/gobe_central/settings_20200719064218.py
+++ .history/gobe_central/settings_20200719064218.py
@@ -96,10 +96,6 @@ DATABASES = {
     }
 }
 
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema',
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
