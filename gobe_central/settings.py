ccc0592cfbb27e90c7e55c793e537f77d4bd4bc0
everybees <jibola.bak@gmail.com>
2020-07-20 16:18:06 +0100

    static files settings

--- gobe_central/settings.py
+++ gobe_central/settings.py
@@ -168,7 +168,7 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # location where you will store your static files
 STATICFILES_DIRS = [
