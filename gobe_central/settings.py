bfa8be7db11ea888576345c0e295dbfe56de0e02
everybees <jibola.bak@gmail.com>
2020-10-15 22:27:31 +0100

    changed DEBUG to True

--- gobe_central/settings.py
+++ gobe_central/settings.py
@@ -33,7 +33,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'gobe-home.herokuapp.com']
 
