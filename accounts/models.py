147d64bdde2006ab894580dc2cd722e75b4e5c64
everybees <jibola.bak@gmail.com>
2020-10-16 16:07:03 +0100

    deleted accounts_profile

--- accounts/models.py
+++ accounts/models.py
@@ -25,28 +25,6 @@ class User(AbstractUser):
     def __str__(self):
         return self.email + " (" + (" not " if not self.confirmed else "") + "confirmed)"
 
-class Profile(models.Model):
-    """non-auth-related/cosmetic fields"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.CharField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    email_confirmed = models.BooleanField(default=False)
-    
-    # Additional data that belongs to User profile
-    # Examples:
-    # Date_of_birth
-    # Bios, descriptions, taglines
-
-
-#create a Profile and bind to a newly created User
-"""receivers to add a Profile for newly created users"""
-@receiver(post_save, sender=User) 
-def create_user_profile(sender, instance, created, **kwargs):
-     if created:
-         Profile.objects.create(user=instance)
-     instance.profile.save()
-     
 
 class Newsletter(models.Model):
 
