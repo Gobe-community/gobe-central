9d8bf96ad570f29c6e5342755c859ee48daab914
everybees <jibola.bak@gmail.com>
2020-10-16 16:06:00 +0100

    changed email_backend to smtp

--- gobe_central/settings.py
+++ gobe_central/settings.py
@@ -98,6 +98,7 @@ WSGI_APPLICATION = 'gobe_central.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
+# }
 
 DATABASES = {
     'default': env.db(),
